4ba83b6f57392e6d7efcda3d23e584211fd429f2
Yaraslau <ysidarkevich@gmail.com>
2023-05-08 12:01:30 +0300

    ball events

--- Dyeing balls Red/Assets/Scripts/Ball.cs
+++ Dyeing balls Red/Assets/Scripts/Ball.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Ball : MonoBehaviour
 {
+    public UnityEvent BallDeathEvent;
+    public UnityEvent<int> BallGetHitEvent;
+    public UnityEvent<int> BallGetBuffEvent;
+    public UnityEvent BallFullOfBuffEvent;
+
     [SerializeField] private int _startLifeCount;
+    [SerializeField] private int _maxBuffCount;
     [SerializeField] private float _maxHorizontalOffset;
     [SerializeField] private float _maxVerticalOffset;
 
     private int _lifeCount;
+    private int _buffCount;
     private Vector3 _nextPosition;
 
     private void Start()
@@ -16,9 +24,30 @@ public class Ball : MonoBehaviour
         _lifeCount = _startLifeCount;
     }
 
-    public void GetDamage()
+    public void GetHit(int damage)
     {
+        if (_lifeCount - damage > 0)
+        {
+            _lifeCount -= damage;
+            BallGetHitEvent?.Invoke(damage);
 
+            return;
+        }
+        
+        BallDeathEvent?.Invoke();
+    }
+
+    public void GetBuff(int buff)
+    {
+        if (_buffCount + buff < _maxBuffCount)
+        {
+            _buffCount += buff;
+            BallGetBuffEvent?.Invoke(buff);
+
+            return;
+        }
+
+        BallFullOfBuffEvent?.Invoke();
     }
 
     public void MoveTo(Vector3 position)
