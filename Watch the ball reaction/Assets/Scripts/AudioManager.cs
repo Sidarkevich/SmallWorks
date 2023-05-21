8eadc1d28c5a46a81972b8f793f30fa0e01c2f1f
Yaraslau <ysidarkevich@gmail.com>
2023-05-21 15:19:31 +0300

    audio mixers

--- Watch the ball reaction/Assets/Scripts/AudioManager.cs
+++ Watch the ball reaction/Assets/Scripts/AudioManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] private Toggle _toggle; 
     [SerializeField] private AudioSource _soundSource;
     [SerializeField] private AudioSource _musicSource;
+    [SerializeField] private AudioMixerGroup _soundOn;
+    [SerializeField] private AudioMixerGroup _soundOff;
 
     private void OnEnable()
     {
@@ -21,8 +24,8 @@ public class AudioManager : MonoBehaviour
 
     public void ChangeAudioSettings(bool isActive)
     {
-        _soundSource.enabled = isActive;
-        _musicSource.enabled = isActive;
+        _soundSource.outputAudioMixerGroup = isActive ? _soundOn : _soundOff;
+        _musicSource.outputAudioMixerGroup = isActive ? _soundOn : _soundOff;
 
         PlayerPrefs.SetInt("AudioSettings", isActive ? 1 : 0);
     }
