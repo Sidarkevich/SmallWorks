eb4603ddd45e15c545b98357563f9053d1439208
Yaraslau <ysidarkevich@gmail.com>
2023-05-21 12:55:49 +0300

    disable fix

--- Watch the ball reaction/Assets/Scripts/BallSpawner.cs
+++ Watch the ball reaction/Assets/Scripts/BallSpawner.cs
@@ -11,6 +11,7 @@ public class BallSpawner : MonoBehaviour
     [SerializeField] private Ball[] _balls;
     [SerializeField] private Transform _topPoint;
     [SerializeField] private Transform _bottomPoint;
+    [SerializeField] private Transform _ballsParent;
 
     public void StartSpawn()
     {
@@ -27,7 +28,7 @@ public class BallSpawner : MonoBehaviour
         while (true)
         {
             Vector3 rndPosition = new Vector3(_topPoint.transform.position.x, Random.Range(_bottomPoint.transform.position.y, _topPoint.transform.position.y), 0);
-            var ball = Instantiate(_balls[Random.Range(0, _balls.Length)], rndPosition, Quaternion.identity);
+            var ball = Instantiate(_balls[Random.Range(0, _balls.Length)], rndPosition, Quaternion.identity, _ballsParent);
             ball.BallClickedEvent.AddListener(_tracker.OnBallClicked);
 
             yield return new WaitForSeconds(_spawnTime);
