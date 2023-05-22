9cd0a7611c2723b2e4aab2effea5f117cfc4c3fc
Yaraslau <ysidarkevich@gmail.com>
2023-05-22 18:58:52 +0300

    version control update

--- Klondike Solitaire/Assets/Scripts/TableauView.cs
+++ Klondike Solitaire/Assets/Scripts/TableauView.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class TableauView : MonoBehaviour
 {
     [SerializeField] private float _yOffset;
+    [SerializeField] private RectTransform _tableauTransform;
 
-    public void SetupView(List<Card> cads)
+    public void SetupView(List<Card> cards)
     {
-        
+        for (int i = 0; i < cards.Count; i++)
+        {
+            var rect = cards[i].gameObject.GetComponent<RectTransform>();
+
+            rect.SetParent(_tableauTransform);
+            rect.SetLocalPositionAndRotation(new Vector3(0, _yOffset * i, 0), Quaternion.identity);
+        }
     }
 }
