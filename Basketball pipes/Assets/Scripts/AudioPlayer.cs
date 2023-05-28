aeeb2971e06e33bf98b1848da0c6fbe67927248a
Yaraslau <ysidarkevich@gmail.com>
2023-05-28 15:00:37 +0300

    sound settings save

--- Basketball pipes/Assets/Scripts/AudioPlayer.cs
+++ Basketball pipes/Assets/Scripts/AudioPlayer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioPlayer : MonoBehaviour
 {
@@ -11,7 +12,34 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private AudioMixerGroup _mixerOn;
     [SerializeField] private AudioMixerGroup _mixerOff;
 
+    [SerializeField] private Toggle _soundToggle;
+
     [SerializeField] private AudioClip _clickClip;
+
+    private bool _isLoading = true;
+    private int _soundSettings;
+
+    private void Awake()
+    {
+        _soundSettings = PlayerPrefs.GetInt("SoundSettings", 1);
+        ChangeSoundState((_soundSettings > 0)? true : false);
+    }
+
+    private void Start()
+    {
+        _isLoading = false;
+    }
+
+    private void Play(AudioClip clip)
+    {
+        if (_isLoading)
+        {
+            return;
+        }
+
+        _soundSource.clip = clip;
+        _soundSource.Play();
+    }
 
     public void ChangeSoundState(bool value)
     {
@@ -19,11 +47,18 @@ public class AudioPlayer : MonoBehaviour
 
         _soundSource.outputAudioMixerGroup = mixer;
         _musicSource.outputAudioMixerGroup = mixer;
+
+        _soundToggle.isOn = value;
+
+        var newSettings = value ? 1 : 0;
+        if (newSettings != _soundSettings)
+        {
+            PlayerPrefs.SetInt("SoundSettings", newSettings);
+        }
     }
 
     public void PlayClick()
     {
-        _soundSource.clip = _clickClip;
-        _soundSource.Play();
+        Play(_clickClip);
     }
 }
