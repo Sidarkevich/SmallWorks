2def44151c9d4a9c33aac4f498460e86eeae3e23
Yaraslau <ysidarkevich@gmail.com>
2023-06-07 14:42:43 +0300

    cameras test

--- Major/Assets/Scripts/PlayerMovement.cs
+++ Major/Assets/Scripts/PlayerMovement.cs
@@ -9,13 +9,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform[] _targets;
     private int _currentIndex;
 
-    private void OnMouseUpAsButton()
+    private void Start()
     {
         MoveToTarget();
     }
 
     private void MoveToTarget()
     {
-        _player.DOMoveX(_targets[_currentIndex].position.x, 1).OnComplete(() => {_currentIndex++; });
+        _player.DOMoveX(_targets[_currentIndex].position.x, 1).OnComplete(() => {_currentIndex++; MoveToTarget(); });
     }
 }
