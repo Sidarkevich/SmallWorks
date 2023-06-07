2ae5a2aa8621f22a843771e95b675b6f7c9341bb
Yaraslau <ysidarkevich@gmail.com>
2023-06-07 16:23:36 +0300

    some work with level

--- Major/Assets/Scripts/PlayerMovement.cs
+++ Major/Assets/Scripts/PlayerMovement.cs
@@ -9,13 +9,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform[] _targets;
     private int _currentIndex;
 
-    private void Start()
+    public void Move()
     {
         MoveToTarget();
     }
 
     private void MoveToTarget()
     {
-        _player.DOMoveX(_targets[_currentIndex].position.x, 1).OnComplete(() => {_currentIndex++; MoveToTarget(); });
+        _player.DOMoveX(_targets[_currentIndex].position.x, 1).OnComplete(() => {_currentIndex++;});
     }
 }
