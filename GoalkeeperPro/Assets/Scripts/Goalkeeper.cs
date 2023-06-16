96557c5429118f09434772884a9652410ccea1a1
Yaraslau <ysidarkevich@gmail.com>
2023-06-16 23:59:47 +0300

    goalkeper movement

---
+++ GoalkeeperPro/Assets/Scripts/Goalkeeper.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goalkeeper : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    
+    [SerializeField] private Transform _moveEndPoint; 
+
+    private Vector3 _moveDirection = Vector3.zero;
+
+    public void Move(Vector3 direction)
+    {
+        _moveDirection = direction;
+    }
+
+    public void Stop()
+    {
+        _moveDirection = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        var delta = _moveDirection * _moveSpeed * Time.deltaTime;
+
+        if (Mathf.Abs(transform.position.x + delta.x) < _moveEndPoint.position.x)
+        {
+            transform.Translate(delta);
+        }
+    }
+}
---
+++ GoalkeeperPro/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Goalkeeper _keeper;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        var point = Camera.main.ScreenToWorldPoint(eventData.position);
+
+        if (point.x > 0)
+        {
+            _keeper.Move(Vector3.right);
+        }
+        else
+        {
+            _keeper.Move(Vector3.left);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _keeper.Stop();
+    }
+}
