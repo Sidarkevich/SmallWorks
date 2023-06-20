d0fda6cef6cd692bb7b06370cffd419437292de2
Yaraslau <ysidarkevich@gmail.com>
2023-06-20 19:16:54 +0300

    ball input

--- Flying Basketball/Assets/Scripts/Ball.cs
+++ Flying Basketball/Assets/Scripts/Ball.cs
@@ -7,15 +7,17 @@ public class Ball : MonoBehaviour
     [SerializeField] private float _forceValue;
 
     private Vector3 _direction;
-
+    private float _gravity = -9.8f;
+    private float _strength = 5f;
 
     public void Kick()
     {
-        _body.AddForce(Vector2.up*_forceValue, ForceMode2D.Impulse);
+        _direction = Vector3.up * _strength;
     }
 
     private void Update()
     {
-
+        _direction.y += _gravity * Time.deltaTime;
+        transform.position += _direction * Time.deltaTime;
     }
 }
