1304d825e0dfe857850ce387bb5677bba74853e5
Yaraslau <ysidarkevich@gmail.com>
2023-06-21 05:20:22 +0300

    pause mechanic

---
+++ FireFootball/Assets/Scripts/PauseHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseHandler : MonoBehaviour
+{
+    public UnityEvent PausedEvent;
+    public UnityEvent UnpausedEvent;
+
+    private float _startTimeScale;
+
+    private void OnEnable()
+    {
+        Pause();
+    }
+
+    private void OnDisable()
+    {
+        Unpause();
+    }
+
+    private void Awake()
+    {
+        _startTimeScale = Time.timeScale;
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        PausedEvent?.Invoke();
+    }
+
+    private void Unpause()
+    {
+        Time.timeScale = _startTimeScale;
+        UnpausedEvent?.Invoke();
+    }
+}
