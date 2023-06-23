b81ad5529f8a10d28bdf28685fbce9cb7d372535
Yaraslau <ysidarkevich@gmail.com>
2023-06-24 01:59:04 +0300

    flap animation

--- Flying Basketball/Assets/Scripts/Ball.cs
+++ Flying Basketball/Assets/Scripts/Ball.cs
@@ -6,11 +6,13 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _body;
     [SerializeField] private float _strength = 6f;
+    [SerializeField] private Animation _animation;
 
     private bool _needCalculate = true;
 
     public void Kick()
     {
+        _animation.Play();
         _body.velocity = Vector2.up * _strength;
     }
 }
