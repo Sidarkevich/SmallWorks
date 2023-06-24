4ebac7206203e7c855595253eebc4ca3f53df0f1
Yaraslau <ysidarkevich@gmail.com>
2023-06-25 02:53:36 +0300

    ball physic material

--- Flying Basketball/Assets/Scripts/Ball.cs
+++ Flying Basketball/Assets/Scripts/Ball.cs
@@ -8,11 +8,32 @@ public class Ball : MonoBehaviour
     [SerializeField] private float _strength = 6f;
     [SerializeField] private Animation _animation;
 
-    private bool _needCalculate = true;
+    private Transform _startTransform;
+
+    public void ChangeState(bool isAlive)
+    {
+        _body.constraints = (isAlive ? RigidbodyConstraints2D.FreezePositionX : RigidbodyConstraints2D.None);
+
+        if (!isAlive)
+        {
+            _body.AddForce(Vector2.right * 20);
+        }
+    }
 
     public void Kick()
     {
         _animation.Play();
         _body.velocity = Vector2.up * _strength;
+    }
+
+    private void Awake()
+    {
+        _startTransform = transform;
+    }
+
+    private void OnEnable()
+    {
+        transform.position = _startTransform.position;
+        transform.rotation = _startTransform.rotation;
     }
 }
