b83729014004bae3c133fe5bc3ac06b60a916bf3
Yaraslau <ysidarkevich@gmail.com>
2023-07-02 18:03:09 +0300

    level change logic

--- Ulta Darts/Assets/Scripts/Board.cs
+++ Ulta Darts/Assets/Scripts/Board.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Board : MonoBehaviour
 {
-    [SerializeField] private Animation _disappearance;
+    [SerializeField] private Animation _animation;
+    [SerializeField] private AnimationClip _disappearanceClip;
+    [SerializeField] private AnimationClip _appearanceClip;
+
+    private UnityEvent _disappearedEvent = new UnityEvent();
 
     public void Disappear()
     {
-        _disappearance.Play();
+        _animation.clip = _disappearanceClip;
+        _animation.Play();
+    }
+
+    public void AddDisappearedListener(UnityAction action)
+    {
+        _disappearedEvent.AddListener(action);
+    }
+
+    public void InvokeDisappearedEvent()
+    {
+        _disappearedEvent?.Invoke();
+        _disappearedEvent.RemoveAllListeners();
+    }
+
+    private void OnEnable()
+    {
+        _animation.clip = _appearanceClip;
+        _animation.Play();
     }
 }
--- Ulta Darts/Assets/Scripts/LevelHandler.cs
+++ Ulta Darts/Assets/Scripts/LevelHandler.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LevelHandler : MonoBehaviour
 {
+    [SerializeField] private UnityEvent _levelPassedEvent;
     [SerializeField] private List<LevelData> _levels;
     [SerializeField] private Animation _levelAnimation;
 
     private LevelData _currentLevel;
     private Transform _boardsParent;
     private Board[] _boards;
+    private int _activeCount;
+
+    public void ChangeToRandomLevel()
+    {
+        ChangeLevel(Random.Range(1, _levels.Count));
+    }
 
     private void OnEnable()
     {
@@ -19,19 +27,42 @@ public class LevelHandler : MonoBehaviour
     private void Awake()
     {
         _boardsParent = transform;
-        _boards = _boardsParent.GetComponentsInChildren<Board>();
+        _boards = _boardsParent.GetComponentsInChildren<Board>(true);
     }
 
     private void ChangeLevel(int index)
     {
+        foreach (var board in _boards)
+        {
+            board.gameObject.SetActive(false);
+        }
+
         _currentLevel = _levels[index];
 
         for (int i = 0; i < _boards.Length; i++)
         {
-            _boards[i].gameObject.SetActive(i < _currentLevel.ActiveCount);
+            var needActivate = (i < _currentLevel.ActiveCount);
+
+            _boards[i].gameObject.SetActive(needActivate);
+
+            if (needActivate)
+            {
+                _boards[i].AddDisappearedListener(OnBoardDisappeared);
+            }
         }
 
         _levelAnimation.clip = _currentLevel.MovementClip;
         _levelAnimation.Play();
+        _activeCount = _currentLevel.ActiveCount;
+    }
+
+    private void OnBoardDisappeared()
+    {
+        _activeCount--;
+
+        if (_activeCount <= 0)
+        {
+            _levelPassedEvent?.Invoke();
+        }
     }
 }
