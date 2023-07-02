4ccc9b6dd175d2bea58b94cc05207f8fdd141a14
Yaraslau <ysidarkevich@gmail.com>
2023-07-02 22:49:23 +0300

    loss timer

--- Ulta Darts/Assets/Scripts/HitCheck.cs
+++ Ulta Darts/Assets/Scripts/HitCheck.cs
@@ -24,6 +24,7 @@ public class HitCheck : MonoBehaviour
             }
         }
 
+        _score.Loss();
         return false;
     }
 }
---
+++ Ulta Darts/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    public UnityEvent StartedEvent;
+    public UnityEvent FinishedEvent;
+
+    [SerializeField] private float _duration;
+
+    public void Start()
+    {
+        StartedEvent?.Invoke();
+        StartCoroutine(AnimationCoroutine());
+    }
+
+    private IEnumerator AnimationCoroutine()
+    {
+        yield return new WaitForSeconds(_duration);
+
+        FinishedEvent?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+}
