e4bec62dc26b42add20575fd092428d240d32654
Yaraslau <ysidarkevich@gmail.com>
2023-07-04 01:39:17 +0300

    return arrow

--- Archer Trial/Assets/Scripts/Arrow.cs
+++ Archer Trial/Assets/Scripts/Arrow.cs
@@ -7,18 +7,40 @@ public class Arrow : MonoBehaviour
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private float _maxForce;
     
+    private Vector2 _startPosition;
+    private Quaternion _startRotation;
+    private bool _isFlying;
+
     public void Shoot(Vector2 mousePos)
     {
+        _startPosition = transform.position;
+        _startRotation = transform.rotation;
+
         var direction = new Vector2(transform.position.x, transform.position.y) - mousePos;
 
+        _isFlying = true;
         _rb.simulated = true;
         _rb.AddForce(direction * 4.1f, ForceMode2D.Impulse);
     }
 
     private void Update()
     {
+        if (!_isFlying)
+        {
+            return;
+        }
+
         Vector2 moveDirection = _rb.velocity;
         float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void OnBecameInvisible()
+    {
+        _isFlying = false;
+        _rb.simulated = false;
+        transform.position = _startPosition;
+        transform.rotation = _startRotation;
+        _rb.velocity = Vector2.zero;
     }
 }
