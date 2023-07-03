328d8fd7dd4c68e8c5078ba5d17ab97ec5bde425
Yaraslau <ysidarkevich@gmail.com>
2023-07-04 01:14:28 +0300

    rotation update

--- Archer Trial/Assets/Scripts/Arrow.cs
+++ Archer Trial/Assets/Scripts/Arrow.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _maxForce;
     
     public void Shoot(Vector2 mousePos)
     {
-        Debug.Log("Shoot!");
         var direction = new Vector2(transform.position.x, transform.position.y) - mousePos;
-        var forceValue = direction.magnitude;
 
-        _rb.velocity = (direction * forceValue);
+        _rb.simulated = true;
+        _rb.AddForce(direction * 4.1f, ForceMode2D.Impulse);
+    }
+
+    private void Update()
+    {
+        Vector2 moveDirection = _rb.velocity;
+        float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
