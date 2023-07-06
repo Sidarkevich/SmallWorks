cd45f22e24fb749b52028aa56cbf9c5a0b53f8c6
Yaraslau <ysidarkevich@gmail.com>
2023-07-06 20:46:24 +0300

    jump coeff works

--- Super Cat/Assets/Scripts/GroundCkeck.cs
+++ Super Cat/Assets/Scripts/GroundCkeck.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class GroundCkeck : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
+    private const string _groundParameter = "isGrounded";
+
     public bool IsGrounded => _isGrounded;
     private bool _isGrounded;
 
     private void OnCollisionEnter2D(Collision2D collider)
     {
         _isGrounded = true;
+        _animator.SetBool(_groundParameter, _isGrounded);
     }
 
     private void OnCollisionExit2D(Collision2D collider)
     {
         _isGrounded = false;
+        _animator.SetBool(_groundParameter, _isGrounded);
     }
 }
