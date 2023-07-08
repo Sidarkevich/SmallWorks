7f7450efc3d507ca17e3f2773d3687eef0f11f7f
Yaraslau <ysidarkevich@gmail.com>
2023-07-08 19:04:04 +0300

    sounds

---
+++ Super Cat/Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ToggleButton : MonoBehaviour
+{
+    public UnityEvent<bool> ValueChangedEvent;
+
+    [SerializeField] private Image _OnImage;
+    [SerializeField] private Image _OffImage;
+    [SerializeField] private Button _button;
+
+    private bool _isOn = true;
+
+    public void Clicked()
+    {
+        _isOn = !_isOn;
+        Setup();
+
+        ValueChangedEvent?.Invoke(!_isOn);
+    }
+
+    public void SetValue(bool value)
+    {
+        _isOn = !value;
+        Setup();
+    }
+
+    public void ChangeValue()
+    {
+        _isOn = !_isOn;
+        Setup();
+    }
+
+    private void Setup()
+    {
+        _button.targetGraphic = (_isOn ? _OnImage : _OffImage);
+        _OnImage.gameObject.SetActive(_isOn);
+        _OffImage.gameObject.SetActive(!_isOn);
+    }
+}
