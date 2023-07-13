95f06edbf8f960863154af89ffac228c2ac8727a
Yaraslau <ysidarkevich@gmail.com>
2023-07-14 01:34:17 +0300

    input validation

--- Space Run/Assets/Scripts/RocketInput.cs
+++ Space Run/Assets/Scripts/RocketInput.cs
@@ -6,17 +6,24 @@ using UnityEngine.EventSystems;
 public class RocketInput : MonoBehaviour, IDragHandler
 {
     [SerializeField] private Transform _rocket;
+    [SerializeField] private Transform _rightBorder;
+    [SerializeField] private Transform _leftBorder;
 
-    private Vector3 startTransform = new Vector3();
+    private Vector3 newPosition = new Vector3();
 
     public void OnDrag(PointerEventData eventData)
     {
-        startTransform.x = eventData.pointerCurrentRaycast.worldPosition.x;
-        transform.position = startTransform;
+        if (eventData.pointerCurrentRaycast.isValid)
+        {
+            newPosition.x = eventData.pointerCurrentRaycast.worldPosition.x;
+            newPosition.x = (newPosition.x > 0 ? Mathf.Min(newPosition.x, _rightBorder.position.x) : Mathf.Max(newPosition.x, _leftBorder.position.x));
+
+            transform.position = newPosition;
+        }
     }
 
     private void Start()
     {
-        startTransform = transform.position;
+        newPosition = transform.position;
     }
 }
