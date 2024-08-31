ff55f738ca4168ceec9fc62ce7ecc80eab4de7f9
Yaraslau <ysidarkevich@gmail.com>
2024-09-01 00:57:26 +0200

    tween

--- Slavic/Assets/Scripts/InputListener.cs
+++ Slavic/Assets/Scripts/InputListener.cs
@@ -6,31 +6,9 @@ using UnityEngine;
 public class InputListener : MonoBehaviour
 {
     [SerializeField] private Player player;
-    [SerializeField] private float stepDuration;
-
-    private bool _isFirstRun = true;
     
-    private IEnumerator Start()
-    {
-        while (true)
-        {
-            if (!_isFirstRun)
-            {
-                player.StepNext();   
-            }
-
-            _isFirstRun = false;
-            yield return new WaitForSeconds(stepDuration);   
-        }
-    }
-
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            player.StepNext();
-        }
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             player.StepBack();
--- Slavic/Assets/Scripts/Player.cs
+++ Slavic/Assets/Scripts/Player.cs
@@ -5,17 +5,21 @@ using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private CellMap map;
+    [SerializeField] private float stepDuration;
 
     private Cell currentCell => _steps.LastOrDefault();
-    
+
     private List<Cell> _steps = new List<Cell>();
     private Cell _nextStep;
+    
+    private bool _isFirstRun = true;
 
-    private bool _stepped;
+    private Sequence _movingSequence;
     
     public void StepNext()
     {
@@ -34,17 +38,13 @@ public class Player : MonoBehaviour
 
         _nextStep = GetNextStep();
         _nextStep.View.SetNextStepState();
-
-        _stepped = false;
     }
 
     public void StepBack()
     {
-        if (_steps.Count < 2 || _stepped)
+        if (_steps.Count < 2 )
             return;
 
-        _stepped = true;
-        
         _nextStep.View.SetFreeState();
         currentCell.View.SetFreeState();
         currentCell.IsFree = true;
@@ -61,8 +61,17 @@ public class Player : MonoBehaviour
     private void Start()
     {
         Restart();
+
+        _movingSequence.Append(transform.DOMove(_nextStep.transform.position, stepDuration).OnComplete(Move));
     }
 
+    private void Move()
+    {
+        Debug.LogError("Move");
+        StepNext();
+        _movingSequence.Append(transform.DOMove(_nextStep.transform.position, stepDuration).OnComplete(Move));
+    }
+    
     private Cell GetNextStep()
     {
         var possibleSteps = map.GetFreeNeighbors(currentCell.QRS);
